c974cae813d524752b8c6c9e76acfd2e3dc346c2
Levy <levymcruz@gmail.com>
2023-09-22 18:31:54 -0300

    adding ticketsTypes into seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import dayjs from "dayjs";
+import { faker } from '@faker-js/faker'
 const prisma = new PrismaClient();
 
 async function main() {
@@ -17,6 +18,25 @@ async function main() {
   }
 
   console.log({ event });
+
+  const numInserts = 10; 
+  const ticketTypes = ["Standard", "VIP", "Premium", "Student", "Family"];
+
+  for (let i = 0; i < numInserts; i++) {
+    const name = faker.helpers.arrayElement(ticketTypes);
+    const price = faker.datatype.number({ min: 50, max: 200 });
+    const isRemote = faker.datatype.boolean();
+    const includesHotel = faker.datatype.boolean();
+
+    await prisma.ticketType.create({
+      data: {
+        name,
+        price,
+        isRemote,
+        includesHotel,
+      },
+    });
+  }
 }
 
 main()
