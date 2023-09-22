7aadd07764fe3792d50553101e4f081620dd6fa4
Levy <levymcruz@gmail.com>
2023-09-22 18:17:09 -0300

    finishinr tickettype route

--- src/controllers/tickets-controller.ts
+++ src/controllers/tickets-controller.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
-import { userService } from '@/services';
+import { ticketsService } from '@/services';
+import logger from '@/config/logger';
 
 export async function getTickets(req: Request, res: Response) {
   const { email, password } = req.body;
 
-  const tickets = await userService.createUser({ email, password });
+  return res.json("'tickets'");
+}
 
-  return res.json(tickets);
+export async function getTicketsType(req: Request, res: Response) {
+  logger.info('getTicketsType START');
+  const ticketsTypes = await ticketsService.returnTicketsTypes();
+  logger.info('getTicketsType END');
+  return res.json(ticketsTypes);
 }
