922a83f4a859541d77867c8411e037a07c8ca4d5
Levy <levymcruz@gmail.com>
2023-10-05 17:19:51 -0300

    att

--- tests/unit/booking.unit.test.ts
+++ tests/unit/booking.unit.test.ts
@@ -1,29 +1,41 @@
+import { TicketStatus } from '@prisma/client';
 import { mockEnrollment2 } from './mocks/enrollment.mock';
-import { enrollmentRepository, ticketsRepository } from '@/repositories';
-import { mockTicket } from './mocks/ticket.mock';
+import { mockTicket, mockTicketType } from './mocks/ticket.mock';
+import { bookingService } from '@/services/booking-service';
 
-describe('Unit Tests route /booking', () => {
+describe('Unit Tests Service /booking', () => {
   it('findEnrollmentByUserId should return Enrollment', async () => {
     const input = 456;
-    const mock = jest.spyOn(enrollmentRepository, 'findWithAddressByUserId');
+    const mock = jest.spyOn(bookingService, 'findEnrollmentByUserId');
     mock.mockImplementation((): any => {
       return mockEnrollment2;
     });
-    const enrollment = await enrollmentRepository.findWithAddressByUserId(input);
-    expect(enrollmentRepository.findWithAddressByUserId).toBeCalledTimes(1);
-    expect(enrollmentRepository.findWithAddressByUserId).toHaveBeenCalledWith(input);
+    const enrollment = await bookingService.findEnrollmentByUserId(input);
+    expect(bookingService.findEnrollmentByUserId).toBeCalledTimes(1);
+    expect(bookingService.findEnrollmentByUserId).toHaveBeenCalledWith(input);
     expect(enrollment).toEqual(mockEnrollment2);
   });
 
   it('findTicketByEnrollmentId should return Ticket', async () => {
     const input = 3;
-    const mock = jest.spyOn(ticketsRepository, 'findTicketByEnrollmentId');
+    const mock = jest.spyOn(bookingService, 'findTicketByEnrollmentId');
     mock.mockImplementation((): any => {
       return mockTicket;
     });
-    const enrollment = await ticketsRepository.findTicketByEnrollmentId(input);
-    expect(ticketsRepository.findTicketByEnrollmentId).toBeCalledTimes(1);
-    expect(ticketsRepository.findTicketByEnrollmentId).toHaveBeenCalledWith(input);
-    expect(enrollment).toEqual(mockTicket);
+    const ticket = await bookingService.findTicketByEnrollmentId(input);
+    expect(bookingService.findTicketByEnrollmentId).toBeCalledTimes(1);
+    expect(bookingService.findTicketByEnrollmentId).toHaveBeenCalledWith(input);
+    expect(ticket).toEqual(mockTicket);
+  });
+
+  it('isInvalidBooking should return false', async () => {
+    const mock = jest.spyOn(bookingService, 'isInvalidBooking');
+    mock.mockImplementation((): any => {
+      return true;
+    });
+    const boolean = bookingService.isInvalidBooking(mockTicket, mockTicketType);
+    expect(bookingService.isInvalidBooking).toBeCalledTimes(1);
+    expect(bookingService.isInvalidBooking).toHaveBeenCalledWith(mockTicket, mockTicketType);
+    expect(boolean).toBeFalsy();
   });
 });
--- tests/unit/mocks/ticket.mock.ts
+++ tests/unit/mocks/ticket.mock.ts
@@ -1,12 +1,23 @@
-import { Ticket, TicketStatus } from '@prisma/client';
+import { Ticket, TicketStatus, TicketType } from '@prisma/client';
 
 type TicketMockType = Ticket;
+type TicketTypeMockType = TicketType;
 
 export const mockTicket: TicketMockType = {
   id: 1,
   ticketTypeId: 2,
   enrollmentId: 3,
   status: TicketStatus.RESERVED,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
+export const mockTicketType: TicketTypeMockType = {
+  id: 2,
+  name: 'Ingresso Padrão',
+  price: 1000,
+  isRemote: false,
+  includesHotel: true,
   createdAt: new Date(),
   updatedAt: new Date(),
 };
