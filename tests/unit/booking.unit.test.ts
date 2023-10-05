2ef1cb2709a4a79c293c202c76b021d02c505be5
Levy <levymcruz@gmail.com>
2023-10-05 20:31:04 -0300

    100% with unit tests for getBooking

--- tests/unit/booking.unit.test.ts
+++ tests/unit/booking.unit.test.ts
@@ -1,10 +1,12 @@
+import { TicketStatus } from '@prisma/client';
 import { mockEnrollment1, mockEnrollment2 } from './mocks/enrollment.mock';
 import { generateRandomTicket, generateRandomTicketType } from './mocks/ticket.mock';
 import { bookingService } from '@/services/booking-service';
 import { notFoundError } from '@/errors';
 import { bookingRepository, enrollmentRepository, ticketsRepository } from '@/repositories';
 import { cannotListHotelsError } from '@/errors/cannot-list-hotels-error';
-import { TicketStatus } from '@prisma/client';
+import { generateRoomMock } from './mocks/room.mock';
+import { generateBookingMock } from './mocks/booking.mock';
 
 beforeEach(() => {
   jest.clearAllMocks();
@@ -12,9 +14,7 @@ beforeEach(() => {
 
 describe('Unit Tests Service /booking', () => {
   it('findEnrollmentByUserId should return Enrollment', async () => {
-    jest.spyOn(enrollmentRepository, 'findWithAddressByUserId').mockImplementation((): any => {
-      return mockEnrollment2;
-    });
+    jest.spyOn(enrollmentRepository, 'findWithAddressByUserId').mockResolvedValueOnce(mockEnrollment2);
 
     const enrollment = await bookingService.findEnrollmentByUserId(1);
     expect(enrollment).toEqual(mockEnrollment2);
@@ -100,7 +100,7 @@ describe('Unit Tests Service /booking', () => {
     );
   });
 
-  it('getBooking', async () => {
+  it('getBooking should return notFoundError when booking is null', async () => {
     const mockTicket = generateRandomTicket();
     const mockTicketType = generateRandomTicketType();
     mockTicketType.isRemote = false;
@@ -120,5 +120,38 @@ describe('Unit Tests Service /booking', () => {
     jest.spyOn(bookingService, 'findTicketByEnrollmentId').mockResolvedValueOnce(mockData);
     jest.spyOn(bookingRepository, 'findBooking').mockResolvedValueOnce(null);
     expect(bookingService.getBooking).rejects.toEqual(notFoundError());
+  });
+
+  it('getBooking should return notFoundError when booking is null', async () => {
+    const mockBooking = generateBookingMock();
+    const mockRoom = generateRoomMock()
+    const mockTicket = generateRandomTicket();
+    const mockTicketType = generateRandomTicketType();
+    mockTicketType.isRemote = false;
+    mockTicketType.includesHotel = true;
+    const mockData = {
+      id: mockTicket.id,
+      ticketTypeId: mockTicketType.id,
+      enrollmentId: 1,
+      status: TicketStatus.PAID,
+      createdAt: mockTicket.createdAt,
+      updatedAt: mockTicket.updatedAt,
+      TicketType: mockTicketType,
+    };
+    jest.spyOn(enrollmentRepository, 'findWithAddressByUserId').mockResolvedValueOnce(mockEnrollment2);
+    jest.spyOn(bookingService, 'findEnrollmentByUserId').mockResolvedValueOnce(mockEnrollment1);
+    jest.spyOn(ticketsRepository, 'findTicketByEnrollmentId').mockResolvedValueOnce(mockData);
+    jest.spyOn(bookingService, 'findTicketByEnrollmentId').mockResolvedValueOnce(mockData);
+    jest.spyOn(bookingRepository, 'findBooking').mockResolvedValueOnce({
+      ...mockBooking,
+      Room: mockRoom,
+    });
+    const getBookingReturn = await bookingService.getBooking(1);
+    expect(getBookingReturn).toEqual(
+      expect.objectContaining({
+        id: mockBooking.id,
+        Room: mockRoom,
+      }),
+    );
   });
 });
