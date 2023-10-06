51eebe2f7440bfc68b47a9b73089584e7f9669ca
Levy <levymcruz@gmail.com>
2023-10-06 14:29:11 -0300

    att

--- src/routers/booking-router.ts
+++ src/routers/booking-router.ts
@@ -4,6 +4,6 @@ import { getBooking, postBooking } from '@/controllers/booking-controller';
 
 const bookingRouter = Router();
 
-bookingRouter.get('/', getBooking).post('/', postBooking); //.all('/*', authenticateToken)
+bookingRouter.all('/*', authenticateToken).get('/', getBooking).post('/', postBooking); //.all('/*', authenticateToken)
 
 export { bookingRouter };
